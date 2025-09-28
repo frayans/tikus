37addec21667c4a861d4fdbc70988689b72c4ba8
frayans <102562679+frayans@users.noreply.github.com>
2025-09-28 12:26:59 +0800

    7. Moving Camera Code Into Its Own Class
    - a new `camera` module is intoduced.
    - it will be responsible for two jobs:
        1. construct and dispatch rays into the world
        2. use the results of these rays to construct the rendered image

---
+++ src/camera.rs
@@ -0,0 +1,97 @@
+use std::io::{self, Write};
+
+use crate::{
+    color::{Color, color},
+    hittable::Hittable,
+    math::{INFINITY, Interval, Point3, Vec3, point3, vec3},
+    ray::Ray,
+};
+
+pub struct Camera {
+    pub aspect_ratio: f64,
+    pub img_width: i32,
+}
+
+struct ViewportData {
+    img_width: i32,
+    img_height: i32,
+    center: Point3,
+    pixel00_loc: Point3,
+    pixel_delta_u: Vec3,
+    pixel_delta_v: Vec3,
+}
+
+pub fn render<H: Hittable>(camera: &Camera, world: &H) -> io::Result<()> {
+    let viewport_data = initialize(camera);
+
+    let mut out = io::stdout().lock();
+
+    writeln!(
+        out,
+        "P3\n{} {}\n255",
+        viewport_data.img_width, viewport_data.img_height
+    )?;
+
+    for j in 0..viewport_data.img_height {
+        eprintln!("\rScanlines remaining: {}", j);
+        for i in 0..viewport_data.img_width {
+            let pixel_center = viewport_data.pixel00_loc
+                + (i as f64 * viewport_data.pixel_delta_u)
+                + (j as f64 * viewport_data.pixel_delta_v);
+            let ray_direction = pixel_center - viewport_data.center;
+            let ray = Ray::new(viewport_data.center, ray_direction);
+
+            let pixel_color = ray_color(&ray, world);
+            pixel_color.write_to(&mut out)?;
+        }
+    }
+
+    Ok(())
+}
+
+fn initialize(camera: &Camera) -> ViewportData {
+    let aspect_ratio = camera.aspect_ratio;
+    let img_width = camera.img_width;
+
+    // calculate image height, and ensure that it's at least 1.
+    let img_h = (img_width as f64 / aspect_ratio) as i32;
+    let img_height = if img_h < 1 { 1 } else { img_h };
+
+    // camera
+    let focal_length = 1.0;
+    let viewport_height = 2.0;
+    let viewport_width = viewport_height * (img_width as f64 / img_height as f64);
+    let center = point3(0.0, 0.0, 0.0);
+
+    // calculate the vectors across the horizontal and down the verical viewport edges
+    let viewport_u = vec3(viewport_width, 0.0, 0.0);
+    let viewport_v = vec3(0.0, -viewport_height, 0.0);
+
+    // calculate the horizontal and vertical delta vectors from pixel to pixel
+    let pixel_delta_u = viewport_u / img_width.into();
+    let pixel_delta_v = viewport_v / img_height.into();
+
+    // calculate the location of the upper left pixel
+    let viewport_upper_left =
+        center - vec3(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
+    let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+    ViewportData {
+        img_width,
+        img_height,
+        center,
+        pixel00_loc,
+        pixel_delta_u,
+        pixel_delta_v,
+    }
+}
+
+fn ray_color<H: Hittable>(ray: &Ray, world: &H) -> Color {
+    if let Some(record) = world.hit(ray, Interval::new(0.0, INFINITY)) {
+        0.5 * (record.normal + color(1.0, 1.0, 1.0))
+    } else {
+        let unit_dir = ray.direction().norm();
+        let a = 0.5 * (unit_dir.y() + 1.0);
+        (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
-use std::io;
-use std::io::Write;
-
 use tikus::{
-    color::{Color, color},
-    hittable::Hittable,
+    camera::{Camera, render},
     hittable_list::HittableList,
-    math::{INFINITY, Interval, point3, vec3},
-    ray::Ray,
+    math::point3,
     sphere::Sphere,
 };
 
@@ -16,63 +11,17 @@ fn main() {
     }
 }
 
-fn ray_color<H: Hittable>(ray: &Ray, world: &H) -> Color {
-    if let Some(record) = world.hit(ray, Interval::new(0.0, INFINITY)) {
-        0.5 * (record.normal + color(1.0, 1.0, 1.0))
-    } else {
-        let unit_dir = ray.direction().norm();
-        let a = 0.5 * (unit_dir.y() + 1.0);
-        (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0)
-    }
-}
-
 fn try_main() -> Result<(), Box<dyn std::error::Error>> {
-    let aspect_ratio = 16.0 / 9.0;
-    let img_width = 400;
-
-    // calculate image height, and ensure that it's at least 1.
-    let img_h = (img_width as f64 / aspect_ratio) as i32;
-    let img_height = if img_h < 1 { 1 } else { img_h };
-
     let mut world = HittableList::new();
+
     world.add(Box::new(Sphere::new(point3(0.0, 0.0, -1.0), 0.5)));
     world.add(Box::new(Sphere::new(point3(0.0, -100.5, -1.0), 100.0)));
 
-    // camera
-    let focal_length = 1.0;
-    let viewport_height = 2.0;
-    let viewport_width = viewport_height * (img_width as f64 / img_height as f64);
-    let camera_center = point3(0.0, 0.0, 0.0);
+    let camera = Camera {
+        aspect_ratio: 16.0 / 9.0,
+        img_width: 400,
+    };
 
-    // calculate the vectors across the horizontal and down the verical viewport edges
-    let viewport_u = vec3(viewport_width, 0.0, 0.0);
-    let viewport_v = vec3(0.0, -viewport_height, 0.0);
-
-    // calculate the horizontal and vertical delta vectors from pixel to pixel
-    let pixel_delta_u = viewport_u / img_width.into();
-    let pixel_delta_v = viewport_v / img_height.into();
-
-    // calculate the location of the upper left pixel
-    let viewport_upper_left =
-        camera_center - vec3(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
-    let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
-
-    let mut out = io::stdout().lock();
-
-    writeln!(out, "P3\n{} {}\n255", img_width, img_height)?;
-
-    for j in 0..img_height {
-        eprintln!("\rScanlines remaining: {}", j);
-        for i in 0..img_width {
-            let pixel_center =
-                pixel00_loc + (i as f64 * pixel_delta_u) + (j as f64 * pixel_delta_v);
-            let ray_direction = pixel_center - camera_center;
-            let ray = Ray::new(camera_center, ray_direction);
-
-            let pixel_color = ray_color(&ray, &world);
-            pixel_color.write_to(&mut out)?;
-        }
-    }
-
+    render(&camera, &world)?;
     Ok(())
 }
